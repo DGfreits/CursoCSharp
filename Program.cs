9bf0e9d8b86f22f4cb9671464018d7ebda3d251e
Douglas de Freitas <dgfreitascel@gmail.com>
2021-12-17 13:42:32 -0300

    Estrutura de Controle

--- Program.cs
+++ Program.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 
 using CursoCSharp.Fundamentos;
+using CursoCSharp.EstruturasDeControle;
 
 namespace CursoCSharp {
     class Program {
         static void Main(string[] args) {
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+
+                // FUNDAMENTOS
                 {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
                 {"Comentários - Fudamentos", Comentarios.Executar},
                 {"Váriaveis e Constantes - Fudamentos", VariaveisEConstantes.Executar},
@@ -22,6 +25,11 @@ namespace CursoCSharp {
                 {"Operadores de Atribuição - Fudamentos", OperadorAtribuicao.Executar},
                 {"Operadores Unários - Fudamentos", OperadoresUnarios.Executar},
                 {"Operador Ternári - Fudamentos", OperadorTernario.Executar},
+
+                // ESTRUTURA DE CONTROLE
+                {"Estrutura IF - Estruturas de Controle", EstruturaIf.Executar},
+                {"Estrutura IF/ELSE - Estruturas de Controle", EstruturaIfElse.Executar},
+                {"Estrutura IF/ELSE/IF - Estruturas de Controle", EstruturaIfElseIf.Executar},
             });
              
             central.SelecionarEExecutar();
